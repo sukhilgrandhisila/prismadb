8aa1713101b69a48ffe1c3dc946e044d4061a45e
sukhilgrandhisila <sukhilgrandhisila@gmail.com>
2025-06-30 15:38:38 +0530

    Your new commit message

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
+import { UsersService } from './users/users.service';
+import { PostsService } from './post/post.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService, PrismaService, UsersService, PostsService]
 })
 export class AppModule {}
